0759a1dd0a116874f36f8a90d6d2308cdf80d161
ghaith82 <ghaith.alkayyem@gmail.com>
2020-08-24 02:00:53 -0400

    plz

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,6 +3,6 @@
 echo "********************"
 echo "****Building********"
 echo "********************"
-
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/mavvvv
 echo "Current workign dir is: $PWD"
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
