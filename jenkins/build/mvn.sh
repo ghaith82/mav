e83008e2a5d375de5fda72b3b68d0559c21681e3
ghaith82 <ghaith.alkayyem@gmail.com>
2020-08-24 01:14:20 -0400

    PWDD

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,5 @@ echo "********************"
 echo "****Building********"
 echo "********************"
 
-echo "Current workign dir is: $PWd"
+echo "Current workign dir is: $PWD"
 docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
